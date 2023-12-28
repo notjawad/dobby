56efa1e0d8ec3f1c967fec853cd122c8f38b2724
notjawad <bawskiya@gmail.com>
2023-12-27 21:24:28 -0600

    ♻️ (github_api.py): handle unsuccessful responses with error logging for better debugging ✨ (github_api.py): add method to fetch the latest commit from a repository for further processing

--- utils/github_api.py
+++ utils/github_api.py
@@ -14,34 +14,40 @@ class GitHubAPI:
         async with self.session.get(url) as response:
             if response.status == 200:
                 return await response.json()
-            else:
-                self.logger.error(
-                    f"Failed to fetch PR details for {repo}#{pr_number}: {response.status}"
-                )
-                return None
+            self.logger.error(
+                f"Failed to fetch PR details for {repo}#{pr_number}: {response.status}"
+            )
+            return None
 
     async def fetch_pr_comments(self, repo: str, pr_number: int) -> dict:
         url = f"{self.BASE_URL}/repos/{repo}/pulls/{pr_number}/comments"
         async with self.session.get(url) as response:
             if response.status == 200:
                 return await response.json()
-            else:
-                self.logger.error(
-                    f"Failed to fetch PR comments for {repo}#{pr_number}: {response.status}"
-                )
-                return None
+            self.logger.error(
+                f"Failed to fetch PR comments for {repo}#{pr_number}: {response.status}"
+            )
+            return None
 
     async def fetch_issue_details(self, repo: str, issue_number: int) -> dict:
         url = f"{self.BASE_URL}/repos/{repo}/issues/{issue_number}"
         async with self.session.get(url) as response:
             if response.status == 200:
                 return await response.json()
-            else:
-                self.logger.error(
-                    f"Failed to fetch issue details for {repo}#{issue_number}: {response.status}"
-                )
-                return None
+            self.logger.error(
+                f"Failed to fetch issue details for {repo}#{issue_number}: {response.status}"
+            )
+            return None
+
+    async def fetch_latest_commit(self, repo: str) -> dict:
+        url = f"{self.BASE_URL}/repos/{repo}/commits"
+        async with self.session.get(url) as response:
+            if response.status == 200:
+                return (await response.json())[0]
+            self.logger.error(
+                f"Failed to fetch latest commit for {repo}: {response.status}"
+            )
+            return None
 
     async def close(self):
-        """Close the aiohttp session."""
         await self.session.close()
