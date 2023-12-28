f9de27e0daa0c695e2610a36b6bef18349e93068
notjawad <bawskiya@gmail.com>
2023-12-27 22:03:18 -0600

    ✨ (github.py): add a new command `_commits` to get the latest commits for a repository   🎨 (github.py): improve the formatting of the commit descriptions in the embed   ✨ (github.py): add a `CommitSelectMenu` UI component to allow users to select a commit for more information

--- cogs/github.py
+++ cogs/github.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from constants import *
 from utils.formatting import build_pr_embed
 from utils.github_api import GitHubAPI
+from utils.ui import CommitSelectMenu
 
 
 class Github(commands.Cog):
@@ -59,6 +60,71 @@ class Github(commands.Cog):
         view.add_item(open_in_github)
 
         await ctx.respond(embed=embed, view=view)
+
+    @_git.command(
+        name="commits",
+        description="Get the latest commits for a repository.",
+    )
+    async def _commits(
+        self,
+        ctx: discord.ApplicationContext,
+        repo: discord.Option(
+            str,
+            "The repository to get the latest commits for. (e.g. uni-bot/uni)",
+            required=True,
+        ),
+    ):
+        if repo.count("/") != 1:
+            return await ctx.respond(
+                "Invalid repository format. The format should be **owner/repo**."
+            )
+
+        commits = await self.github_api.fetch_latest_commits(repo)
+
+        if isinstance(commits, str):
+            return await ctx.respond(commits)
+
+        description = []
+
+        for commit in commits:
+            sha = commit["sha"][:7]
+            message = commit["commit"]["message"].split("\n")[0]
+
+            if len(message) > 50:
+                message = f"{message[:50]}..."
+            description.append(
+                f"{EMOJIS['check']} [`{sha}`]({commit['html_url']}) {message.replace('`', '')}"
+            )
+
+        embed = discord.Embed(
+            title=f"{EMOJIS['github']} Latest commits in `{repo}`",
+            description="\n".join(description),
+            color=COLORS["green"],
+            url=f"https://github.com/{repo}",
+        )
+
+        select_menu = CommitSelectMenu(
+            commits,
+            placeholder="Select a commit to view more information.",
+            min_values=1,
+            max_values=1,
+        )
+
+        for commit in commits:
+            sha = commit["sha"][:7]
+            select_menu.add_option(
+                label=f"{sha} - {commit['commit']['author']['name']}",
+                value=sha,
+                description=commit["commit"]["message"].split("\n")[0][:100],
+            )
+
+        view = discord.ui.View()
+        view.add_item(select_menu)
+
+        await ctx.respond(
+            embed=embed,
+            view=view,
+        )
 
 
 def setup(bot_: discord.Bot):
